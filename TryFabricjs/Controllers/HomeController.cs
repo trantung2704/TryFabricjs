59cfadeb186f57fc55faacf8bf11d1a95545f654
DESKTOP-BGBNHS7\Beng <trantung04@gmail.com>
2017-03-15 14:46:22 +0700

    remove unused

--- TryFabricjs/Controllers/HomeController.cs
+++ TryFabricjs/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace TryFabricjs.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page";
 
             return View();
         }
